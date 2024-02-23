fb4370db9f6f2b5f34013876d644c556bf74ad29
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-23 21:00:48 +0700

    first commit

---
+++ next-cookie-session/README.md
@@ -0,0 +1,9 @@
+# Refer
+
+[Lama Dev Next.js Auth](https://www.youtube.com/watch?v=p_FiVGxyksI)
+
+# Install
+
+```
+npx create-next-app@latest
+```
---
+++ next-cookie-session/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to the homepage</h1>
+      <p>This page is visible to everyone!</p>
+    </div>
+  );
+}
