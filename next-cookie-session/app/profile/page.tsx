dde62aea0c152c5143e6508b878ae561a22a05c9
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-24 13:23:03 +0700

    Next.js Protected Routes with Cookie Session

--- next-cookie-session/app/profile/page.tsx
+++ next-cookie-session/app/profile/page.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { getSession } from "../iron-session/app-router-server-component-and-action/actions";
+import { redirect } from "next/navigation";
 
 type Props = {};
 
-export default function Page({}: Props) {
+export default async function Page({}: Props) {
+  const session = await getSession();
+
+  if (!session.isLoggedIn) {
+    redirect("/");
+  }
+
   return (
     <div className="profile">
       <h1>Welcome to the ProfilePage</h1>
+      <p>
+        Welcome, <b>{session.username}</b>
+      </p>
+      <span>
+        You are a <b>{session.isPro ? "Premium" : "Free"}</b> user
+      </span>
     </div>
   );
 }
